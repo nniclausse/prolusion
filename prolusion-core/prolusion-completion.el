6216dad2b7f07805781ba7d78c362dca9f255b4e
Julien Wintz <julien.wintz@inria.fr>
2016-12-16 22:50:12 +0100

    Adding anaconda mode to completion layer.

--- prolusion-core/prolusion-completion.el
+++ prolusion-core/prolusion-completion.el
@@ -20,6 +20,7 @@
 (prolusion-require-package 'company-jedi)
 (prolusion-require-package 'company-irony)
 (prolusion-require-package 'company-irony-c-headers)
+(prolusion-require-package 'company-anaconda)
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Completion configuration
@@ -39,30 +40,35 @@
 (setq company-minimum-prefix-length 1)
 (setq company-tooltip-flip-when-above t)
 
+(setq anaconda-mode-installation-directory (expand-file-name "prolusion-anaconda" prolusion-dir))
+
 (eval-after-load 'company '(add-to-list 'company-backends 'company-irony))
 (eval-after-load 'company '(add-to-list 'company-backends 'company-irony-c-headers))
 (eval-after-load 'company '(add-to-list 'company-backends 'company-cmake))
 (eval-after-load 'company '(add-to-list 'company-backends 'company-jedi))
+(eval-after-load 'company '(add-to-list 'company-backends 'company-anaconda))
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Completion hooks
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(add-hook          'c-mode-hook 'company-mode)
-(add-hook        'c++-mode-hook 'company-mode)
-(add-hook       'objc-mode-hook 'company-mode)
-(add-hook 'emacs-lisp-mode-hook 'company-mode)
-(add-hook      'cmake-mode-hook 'company-mode)
-(add-hook       'html-mode-hook 'company-mode)
-(add-hook        'qml-mode-hook 'company-mode)
-(add-hook        'js2-mode-hook 'company-mode)
-(add-hook     'python-mode-hook 'company-mode)
+(add-hook          'c-mode-hook  'company-mode)
+(add-hook        'c++-mode-hook  'company-mode)
+(add-hook       'objc-mode-hook  'company-mode)
+(add-hook 'emacs-lisp-mode-hook  'company-mode)
+(add-hook      'cmake-mode-hook  'company-mode)
+(add-hook       'html-mode-hook  'company-mode)
+(add-hook        'qml-mode-hook  'company-mode)
+(add-hook        'js2-mode-hook  'company-mode)
+(add-hook     'python-mode-hook  'company-mode)
+(add-hook     'python-mode-hook 'anaconda-mode)
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Completion modeline
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(eval-after-load 'company '(diminish 'company-mode))
+(eval-after-load 'company '(diminish  'company-mode))
+(eval-after-load 'company '(diminish 'anaconda-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
