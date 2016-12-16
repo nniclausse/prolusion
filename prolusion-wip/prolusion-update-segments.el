58f2b606fd1757bc4bc29a63098c9331b66d8e7e
Julien Wintz <julien.wintz@inria.fr>
2016-12-16 01:10:13 +0100

    Pushing WIP progresses.

--- prolusion-wip/prolusion-update-segments.el
+++ prolusion-wip/prolusion-update-segments.el
@@ -14,17 +14,31 @@
 
 (require 'package)
 
+(defvar prolusion-upgrade-count nil)
+
 (defun prolusion-packages/list-packages ()
   ""
   (interactive)
-  (save-window-excursion
-    (package-list-packages)
-    (package-menu-mode)
-    (let ((prolusion--packages-upgrade-count (length (package-menu--find-upgrades)))
-      (kill-buffer (get-buffer "*Packages*")))
-      prolusion--packages-upgrade-count)))
+  (message "so far so good 0")
+  (unless prolusion-upgrade-count
+    (save-window-excursion
+      (package-list-packages)
+      (package-menu-mode)
+      (setq prolusion-upgrade-count (length (package-menu--find-upgrades)))
+      (kill-buffer (get-buffer "*Packages*"))))
+  (message "so far so good")
+  (if (> prolusion-upgrade-count 0)
+      (format " %s"
+              (propertize prolusion-upgrade-count
+                          'mouse-face 'mode-line-highlight
+                          'help-echo (concat (prolusion-upgrade-count)
+                                             "\nmouse-1: Display minor mode menu")
+                          'local-map (let ((map (make-sparse-keymap)))
+                                       (define-key map
+                                         [mode-line down-mouse-1]
+                                         (prolusion-upgrade-packages)))))))
 
-(message " %s" (prolusion-packages/list-packages))
+(message "%s" (prolusion-packages/list-packages))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; prolusion-updates-segment.el ends here
