263c493f410a21879054a5f70e79afca665d0b9a
Julien Wintz <julien.wintz@inria.fr>
2016-12-31 16:39:31 +0100

    Setting up company-dabbrev.

--- prolusion-core/prolusion-completion.el
+++ prolusion-core/prolusion-completion.el
@@ -26,10 +26,11 @@
 ;; Completion configuration
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(setq jedi:complete-on-dot t)
 (setq jedi:environment-root prolusion-jedi-dir)
+
 (setq python-environment-directory prolusion-jedi-dir)
 (setq python-environment-default-root-name "root")
-(setq jedi:complete-on-dot t)
 
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Completion setup
@@ -39,6 +40,7 @@
 (setq company-echo-delay 0.0)
 (setq company-minimum-prefix-length 1)
 (setq company-tooltip-flip-when-above t)
+(setq company-dabbrev-downcase nil)
 
 (setq anaconda-mode-installation-directory (expand-file-name "prolusion-anaconda" prolusion-dir))
 
