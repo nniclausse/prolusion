ac62f1efd1d88c9df11964365b3eeac3685208aa
Julien Wintz <julien.wintz@inria.fr>
2017-01-27 17:03:08 +0100

    Bumped version number.

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
 
 (defconst prolusion-version-major 1)
 (defconst prolusion-version-minor 5)
-(defconst prolusion-version-patch 3)
+(defconst prolusion-version-patch 4)
 
 (defvar prolusion-dir          (file-name-directory load-file-name))
 (defvar prolusion-core-dir     (expand-file-name "prolusion-core"     prolusion-dir))
